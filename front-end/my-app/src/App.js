51f046fa7a9ee7477d6795273894dd9a28b5dcc6
ghareeb <ghareebemails@gmail.com>
2022-12-16 19:52:46 +0200

    update pages

--- front-end/my-app/src/App.js
+++ front-end/my-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+//import Product_List from './components/product/Product_List.jsx';
+import * as ROUTES from './Constants/Route';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './containers/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path={ROUTES.LANDING} element={<Home/>}/>
+        <Route path={ROUTES.HOME} component={Home} />
+      </Routes>
+    </Router>
   );
 }
 
