b2d28f83dbe423171d3a83ba1e3f4bfd396a542d
mohamed <mohamedsharaf2468135@gmail.com>
2022-12-22 01:23:38 +0200

    auth updated login , register done and tested

--- back-end/server.js
+++ back-end/server.js
@@ -1,18 +1,21 @@
 // bndef el express
-const express = require('express');
+const express = require("express");
 // bndef el mongoose
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const PORT = process.env.PORT || 5005;
-const userRouter = require("./routes/user")
-dotenv.config();
+const authRouter = require("./routes/auth");
+const userRouter = require("./routes/user");
 const app = express();
+dotenv.config();
 mongoose.set('strictQuery', true);
 mongoose.connect(
     process.env.DB_URL).then(() => console.log("db connect")).catch((err) => {
     console.log(err);
 });
+
 app.use(express.json());
-app.use("/api/user", userRouter); //we go to api end user our app
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter); //we go to api end user our app
 
 app.listen(PORT, console.log(`server run in ${PORT}`));
