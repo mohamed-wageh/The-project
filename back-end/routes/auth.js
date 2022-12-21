5cad2a95c99b4784a413441e1461694d865822ba
sama <sama34000@gmail.com>
2022-12-21 16:45:52 +0200

    auth update

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,7 +1,8 @@
 const router = require("express").Router();
 const user = require("../models/user")
 const cryptojs = require("crypto-js")
-    //register
+const jwt = require("jsonwebtoken");
+//register
 router.post("/register", async(req, res) => {
     const newuser = new User({
         username: req.body.username,
@@ -24,13 +25,28 @@ router.post("/register", async(req, res) => {
 router.post("/login", async(req, res) => {
     try {
         const user = await User.findOne({ username: req.body.username });
-
+        !user && res.status(401).json("wrong credentials!")
         const hashedpassword = cryptojs.AES.decrypt(
-            user.password, process.env.PASS_SEC);
-        const password = hashedpassword.toString(cryptojs.enc.Utf8);
+            user.password,
+            process.env.PASS_SEC
+        );
+        const originalpassword = hashedpassword.toString(cryptojs.enc.Utf8);
+
+        originalpassword !== req.body.password &&
+            res.status(401).json("wrong credentials!");
+
+        constaccessToken = jwt.sign({
+            id: user._id
+        })
+
+        const { password, ...others } = user._doc;
+
+
+        res.status(200).json(others);
     } catch (err) {
         res.status(500).json(err)
     }
-})
+});
+
 
 module.exports = router;
