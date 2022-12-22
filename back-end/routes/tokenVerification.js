f66413ac32e7fe5f0b8f461f3564caa6358f4e4f
mohamed <mohamedsharaf2468135@gmail.com>
2022-12-22 18:28:00 +0200

    update user done and tested

---
+++ back-end/routes/tokenVerification.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.JWTPRIVATEKEY, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json("Authentication failed");
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json("Access denied");
+    }
+  });
+};
+module.exports = verifyTokenAndAuthorization,verifyToken;
