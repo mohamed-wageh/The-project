ec2fb7b7645802ff8a476ce350fca96c34f63b7f
sama <sama34000@gmail.com>
2022-12-24 15:16:17 +0200

    cart.js is done

--- back-end/routes/cart.js
+++ back-end/routes/cart.js
@@ -29,50 +29,33 @@ router.put("/:id", verifyTokenAndAuthorization, async(req, res) => {
 });
 
 //delete
-router.delete("/:id", verifyTokenAndAdmin, async(req, res) => {
+router.delete("/:id", verifyTokenAndAuthorization, async(req, res) => {
     try {
-        await product.findByIdAndDelete(req.params.id);
-        res.status(200).json("product has been deleted...");
+        await cart.findByIdAndDelete(req.params.id);
+        res.status(200).json("cart has been deleted...");
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-//get product
-router.get("/find/:id", async(req, res) => {
+//get cart
+router.get("/find/:userId", verifyTokenAndAuthorization, async(req, res) => {
     try {
-        const product = await product.findById(req.params.id);
-        res.status(200).json(product);
+        const cart = await cart.findOne({ userId: req.params.userId });
+        res.status(200).json(cart);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-//get all product
-router.get("/", async(req, res) => {
-    const qNew = req.query.new;
-    const qCategory = req.query.category;
+//get all 
+
+router.get("/", verifyTokenAndAdmin, async(req, res) => {
     try {
-        let products;
-
-        if (qNew) {
-            product = await product.find().sort({ createdAt: -1 }).limit(1);
-        } else if (qCategory) {
-            products = await product.find({
-                categories: {
-                    $in: [qCategory],
-                },
-            });
-        } else {
-            products = await product.find();
-        }
-
-
-        res.status(200).json(products);
+        const carts = await cart.find();
+        res.status(200).json(carts);
     } catch (err) {
         res.status(500).json(err);
     }
 });
-
-
 module.exports = router;
