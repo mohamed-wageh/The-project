1d3bf7608cf75c94e73050c972b363360e099606
mohamed <mohamedsharaf2468135@gmail.com>
2022-12-24 22:06:21 +0200

    updated

--- back-end/models/Order.js
+++ back-end/models/Order.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const OrderSchema = new mongoose.Schema(
     {
-        userId:{type: String , required:true},
-        products:[
+        userId:{type: String , required:true, unique:true},
+        Products:[
             {
                 productId:{
                     type:String
@@ -11,12 +11,12 @@ const OrderSchema = new mongoose.Schema(
                 purchasedQuantity:{
                     type:Number, 
                     default:1,
-                },
-            },
+                }
+            }
         ],
         userAddress:{type:Object ,required: true},
-        totalAmount: {type:Number ,required: true},
-        orderStatus:{type:String , default:"Pending"},
+        totalAmount: {type:number ,required: true},
+        orderStatus:{type:String , default:"Pending"}
     },
     //to save the date ex:(created at: time , updated at: time)
     {timestamps: true}
