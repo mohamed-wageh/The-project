18d4f45a0bd7193a03d37de5865541983a5f051d
mohamed <mohamedsharaf2468135@gmail.com>
2022-12-24 16:35:59 +0200

    order done server updated

--- back-end/server.js
+++ back-end/server.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 5005;
 const authRouter = require("./routes/auth");
 const userRouter = require("./routes/user");
 const productRouter = require("./routes/product");
+const cartRouter = require("./routes/cart");
+const orderRouter = require("./routes/order");
 const app = express();
 dotenv.config();
 mongoose.set('strictQuery', true);
@@ -19,4 +21,6 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter); //we go to api end user our app
 app.use("/api/products", productRouter);
+app.use("/api/carts", cartRouter);
+app.use("/api/orders", orderRouter);
 app.listen(PORT, console.log(`server run in ${PORT}`));
