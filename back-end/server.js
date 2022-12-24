5516b7416d83864f974e2acfadc3329f4f50a02e
sama <sama34000@gmail.com>
2022-12-24 20:21:09 +0200

    stripe.js is done and stripe done server updated

--- back-end/server.js
+++ back-end/server.js
@@ -9,6 +9,8 @@ const userRouter = require("./routes/user");
 const productRouter = require("./routes/product");
 const cartRouter = require("./routes/cart");
 const orderRouter = require("./routes/order");
+const stripeRouter = require("./routes/stripe");
+const cors = require("cors");
 const app = express();
 dotenv.config();
 mongoose.set('strictQuery', true);
@@ -17,10 +19,12 @@ mongoose.connect(
     console.log(err);
 });
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter); //we go to api end user our app
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 app.use("/api/orders", orderRouter);
+app.use("/api/checkout", stripeRouter);
 app.listen(PORT, console.log(`server run in ${PORT}`));
